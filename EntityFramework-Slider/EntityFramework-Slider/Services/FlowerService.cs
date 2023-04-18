986eb26984cbb01e2aa7ba6cebc1e2cc42a41302
Alakbarbh <alakbarbh@code.edu.az>
2023-04-18 08:50:08 -0800

    ViewComponents, Category crud

---
+++ EntityFramework-Slider/EntityFramework-Slider/Services/FlowerService.cs
@@ -0,0 +1,26 @@
+﻿using EntityFramework_Slider.Data;
+using EntityFramework_Slider.Models;
+using EntityFramework_Slider.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramework_Slider.Services
+{
+    public class FlowerService : IFlowerService
+    {
+        private readonly AppDbContext _context;
+        public FlowerService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Expert>> GetAll()
+        {
+            return await _context.Experts.ToListAsync();
+        }
+
+        public async Task<ExpertHeader> GetInfo()
+        {
+            return  await _context.ExpertHeaders.FirstOrDefaultAsync();
+        }
+    }
+}
